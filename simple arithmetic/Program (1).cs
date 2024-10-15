c03a78cdc3517c84f07083b6d484ca04f341fa70
Rayne <rava438@student.oaisd.org>
2024-10-15 08:51:06 -0400

    Simple Arithmetic
    Simple Arithmetic

---
+++ simple arithmetic/Program (1).cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int num = 5 + 3;
+        Console.WriteLine(num);
+
+        int num1 = 700 - 300;
+        Console.WriteLine(num1);
+
+        int num2 = 60 * 40;
+        Console.WriteLine(num2);
+
+        int num3 = 45 / 5;
+        Console.WriteLine(num3);
+
+
+        float num4 = 6.0f / 2.0f;
+        Console.WriteLine(num4);
+
+        int result = 5528 % 2;
+        Console.WriteLine(result);
+
+
+
+    }
+
+
+
+}
